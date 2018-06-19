91989b73de1c8cfec503f7a70ed5cb2f0f2fdad5
Gallian <24864749+galliangg@users.noreply.github.com>
2018-06-19 17:46:34 +0100

    More

--- install.sh
+++ install.sh
@@ -35,8 +35,8 @@ set -e
 
 # Here we go.. ask for the administrator password upfront and run a
 # keep-alive to update existing `sudo` time stamp until script has finished
-# sudo -v
-# while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+sudo -v
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Homebrew will install the command-line tools automatically
 
